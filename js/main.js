711052beca47c73126b030c0ba696dae7df09220
KhuyenNH <huukhuyen.dev@gmail.com>
2018-05-14 13:25:02 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 var wBrowser = $(window).width();
 if (wBrowser < 768) {
     var posScroll = 0;
+    $('img').each(function() {
+        $(this).attr("src", $(this).attr("src").replace('_pc', '_sp'));
+    });
 } else {
     var posScroll = 100;
+    $('img').each(function() {
+        $(this).attr("src", $(this).attr("src").replace('_sp', '_pc'));
+    });
 }
+
+var h_section01 = $('.section_01').height();
+$('.paralax1').height(h_section01);
+
+var heightHead = $('.header').outerHeight();
+$('.page-body').css("padding-top", heightHead);
+
 /* Reponsive for meta
 -------------------------------------- */
 $(window).resize(function() {
-    if (screen.width <= 1100 && screen.width > 767) {
+    if (screen.width > 767 && screen.width <= 1100) {
         $('meta[name="viewport"]').attr('content', 'width=1100, user-scalable=no, shrink-to-fit=no');
     } else {
         $('meta[name="viewport"]').attr('content', 'width=device-width, user-scalable=no, shrink-to-fit=no');
     }
 }).resize();
-
-var heightHead = $('.header').height();
-$('.page-body').css("padding-top", heightHead);
-$('.toggle_nav').click(function(event) {
-    $('.nav').stop().slideToggle();
-});
 
 /* Fade scroll
 -------------------------------------- */
